6eff083599896c5b2e89d769193386ae956be83b
Cemetery Corvus <42334422+cemeteryparty@users.noreply.github.com>
2020-05-26 14:12:09 +0800

    addition ALU_op of Branch

--- co-lab/Lab3/code/Decoder.v
+++ co-lab/Lab3/code/Decoder.v
@@ -36,11 +36,13 @@ assign MemRead = (instr_op == 35) ? 1 : 0;
 assign MemWrite = (instr_op == 43) ? 1 : 0;
 assign ALU_op = (instr_op == 4 ? 3'b011 : // BEQ -> SUB
                 (instr_op == 5 ? 3'b011 : // BNE -> SUB
+                (instr_op == 6 ? 3'b011 : // BLEZ -> SUB
+                (instr_op == 7 ? 3'b011 : // BGTZ -> SUB
                 (instr_op == 8 ? 3'b010 : // ADDI -> ADD
                 (instr_op == 11 ? 3'b100 : // SLTIU -> SLTIU
                 (instr_op == 13 ? 3'b001 : // ORI -> OR
                 (instr_op == 15 ? 3'b110 : // LUI -> LUI
-                3'b111))))));
+                3'b111))))))));
 assign ALUSrc = (instr_op == 8 || instr_op == 11 || instr_op == 15 || instr_op == 13);
 assign RegDst = (instr_op == 0);
 assign Branch = (instr_op == 4 || instr_op == 5 || instr_op == 6 || instr_op == 7);
