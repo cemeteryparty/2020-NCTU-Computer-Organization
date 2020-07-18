af5b17b4ce21215ff1bf5db4689b2eaf5ff6b19e
Cemetery Corvus <42334422+cemeteryparty@users.noreply.github.com>
2020-07-18 15:37:47 +0800

    Correct LRU mechanism

--- co-lab/Lab4/0716085_0716316/direct_mapped_cache_lru.cpp
+++ co-lab/Lab4/0716085_0716316/direct_mapped_cache_lru.cpp
@@ -2,16 +2,10 @@
 #include <cstdio>
 #include <cmath>
 #include <vector>
-#include <climits>
 
 using namespace std;
-/*
-vector<float> miss_rates;
-vector<int> blk_sizes;
-vector<int> cach_sizes;
-*/
 
-int n_ways = 2; // 2-ways
+int n_ways = 8;
 
 struct cache_content{
 	bool v;
@@ -21,89 +15,64 @@ struct cache_content{
 };
 
 const int K = 1024;
-
-void simulate(int cache_size, int block_size,char *filename){
-	printf("cache: %d, block: %d\n",cache_size,block_size);
-	unsigned int tag, index, x;
+void simulate(int cache_size, int block_size){
+	int block_count = cache_size / block_size;
+	int line = block_count / n_ways;
 	int offset_bit = (int)log2(block_size);
-	int index_bit = (int)log2(cache_size / block_size);
-	int line = cache_size >> (offset_bit);
+	int index_bit = (int)log2(line);
 
-
-	vector<vector<cache_content> > cache(line,vector<cache_content>(n_ways));
-	
-    printf("cache line: %d\n",line);
-
-    for(int i = 0;i < line;i++)
+	vector<vector<cache_content> > cache(line, vector<cache_content>(n_ways));
+	for(int i = 0;i < line;i++)
 		for(int j = 0; j < n_ways; j++){
-        	cache[i][j].v = false;
-        	cache[i][j].time_stamp = 0;
+			cache[i][j].v = false;
+			cache[i][j].time_stamp = -1;
 		}
 
-    FILE *fp = fopen(filename, "r");  // read file
 	int instr_count = 0,miss_count = 0;
-
+	unsigned int tag, index, x;
+	FILE *fp = fopen("RADIX.txt", "r");  // read file
 	while(fscanf(fp, "%x", &x) != EOF){
-        bool hit = false;
-		index = (x >> offset_bit) & (line - 1);
-		tag = x >> (index_bit + offset_bit);
-        //printf("data = %x\tindex = %d\ttag = %d\n",x,index,tag);
-		int loc = 0,diff = INT_MIN;
-		for(int i = 0;i < n_ways;i++){
-			if(!cache[index][i].v || (cache[index][i].v && cache[index][i].tag != tag)){
-				if((instr_count - cache[index][i].time_stamp) > diff){
-					diff = instr_count - cache[index][i].time_stamp;
-					loc = i;
-				}
-            }
-            else if(cache[index][i].v && cache[index][i].tag == tag){
-                hit = true;
-                cache[index][i].time_stamp = instr_count;
-                break;
-            }
-		}
-		if(!hit){
-            cache[index][loc].v = true;  // miss
-			cache[index][loc].tag = tag;
-			cache[index][loc].time_stamp = instr_count;
-			miss_count++;
-		}
 		instr_count++;
+		bool hit = false;
+		index = (x >> (offset_bit)) & (line - 1);
+		tag = x >> (index_bit + offset_bit);
+		for(int i = 0;i < n_ways;i++)
+			if(cache[index][i].v && cache[index][i].tag == tag){ // hit
+				hit = true;
+				cache[index][i].time_stamp = instr_count;
+				break;
+			}
+		if(!hit){ // miss
+			miss_count++;
+			int lru = 0, longestT = cache[index][0].time_stamp;
+			for(int i = 0;i < n_ways;i++){
+				if(cache[index][i].time_stamp == -1){
+					lru = i;
+					break;
+				}
+				if(cache[index][i].time_stamp < longestT){
+					lru = i;
+					longestT = cache[index][i].time_stamp;
+				}
+			}
+			cache[index][lru].v = true;
+			cache[index][lru].tag = tag;
+			cache[index][lru].time_stamp = instr_count;
+		}
 	}
-	fclose(fp);
-	
 	printf("Miss Rate = %d / %d = %f\n",miss_count,instr_count,(float)miss_count / instr_count);
-	/*
-	cach_sizes.push_back(cache_size);
-    blk_sizes.push_back(block_size);
-    miss_rates.push_back((float)miss_count / instr_count);
-    */
 }
 
-int main()
-{
+int main(int argc, const char *argv[]){
 	// Let us simulate 4KB cache with 16B blocks
 	//simulate(4 * K, 16);
-	char f1[] = "LU.txt",f2[] = "RADIX.txt";
-	printf("%s:\n",f1);
 	printf("%d ways\n",n_ways);
 	for(int i = 4;i <= 256;i <<= 2)
 		for(int j = 16;j <= 256;j <<= 1){
-			if(j != 64)
-				continue;
-			simulate(i * K,j,f1);
+			//if(j != 64)
+			//	continue;
+			printf("cache: %d, block: %d\n",i * K,j);
+			simulate(i * K,j);
 		}
-	printf("%s:\n",f2);
-	printf("%d ways\n",n_ways);
-	for(int i = 4;i <= 256;i <<= 2)
-		for(int j = 16;j <= 256;j <<= 1){
-			if(j != 64)
-				continue;
-			simulate(i * K,j,f2);
-		}	
-	/*
-	FILE *fw = fopen("ans.txt", "w");
-	for(int i = 0; i < miss_rates.size(); i++)
-        fprintf(fw, "%d %d %f\n", cach_sizes[i], blk_sizes[i], miss_rates[i]);
-	*/
+	return 0;
 }
